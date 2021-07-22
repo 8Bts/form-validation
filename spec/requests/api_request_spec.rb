edc7b717d24cd1af44794886c4a239999080fcfc
8Bts <mcmizze@yahoo.com>
2021-07-22 16:38:20 +0400

    Refactor `api_request_spec.rb` to use webmock without VCR

--- spec/requests/api_request_spec.rb
+++ spec/requests/api_request_spec.rb
@@ -1,13 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe 'Post request to /authors', type: :request, vcr: { record: :new_episodes } do
+RSpec.describe 'Post request to /authors', type: :request do
   it 'sends HTTP request with valid data and receives response status 201' do
     data = {
       first_name: 'Tim',
       last_name: 'Cooper',
       email: 'tim@cooper.cm'
     }
-    expect(Api.send_data(data).status).to eql(201)
+    VCR.turned_off do
+      stub_request(:post, "https://gorest.co.in/public/v1/users?access-token=#{Api::AUTH_KEY}")
+        .with(
+          body: '{"name":"Tim Cooper","gender":"male","email":"tim@cooper.cm","status":"active"}',
+          headers: {
+            'Accept' => '*/*',
+            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Content-Type' => 'application/json',
+            'User-Agent' => 'Faraday v1.5.1'
+          }
+        )
+        .to_return(status: 201, body: '', headers: {})
+      expect(Api.send_data(data).status).to eql(201)
+    end
   end
 
   it 'sends HTTP request with invalid data and raises Faraday::UnprocessableEntityError(422)' do
@@ -16,7 +29,20 @@ RSpec.describe 'Post request to /authors', type: :request, vcr: { record: :new_e
       last_name: nil,
       email: 'nil@nil.com'
     }
-    expect { Api.send_data(data) }.to raise_error(Faraday::UnprocessableEntityError)
+    VCR.turned_off do
+      stub_request(:post, "https://gorest.co.in/public/v1/users?access-token=#{Api::AUTH_KEY}")
+        .with(
+          body: '{"name":" ","gender":"male","email":"nil@nil.com","status":"active"}',
+          headers: {
+            'Accept' => '*/*',
+            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Content-Type' => 'application/json',
+            'User-Agent' => 'Faraday v1.5.1'
+          }
+        )
+        .to_return(status: 422, body: '', headers: {})
+      expect { Api.send_data(data).status }.to raise_error(Faraday::UnprocessableEntityError)
+    end
   end
 
   it 'sends HTTP request with already in use email and raises Faraday::UnprocessableEntityError(422)' do
@@ -25,6 +51,19 @@ RSpec.describe 'Post request to /authors', type: :request, vcr: { record: :new_e
       last_name: 'One',
       email: 'tim@cooper.cm'
     }
-    expect { Api.send_data(data) }.to raise_error(Faraday::UnprocessableEntityError)
+    VCR.turned_off do
+      stub_request(:post, "https://gorest.co.in/public/v1/users?access-token=#{Api::AUTH_KEY}")
+        .with(
+          body: '{"name":"Some One","gender":"male","email":"tim@cooper.cm","status":"active"}',
+          headers: {
+            'Accept' => '*/*',
+            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Content-Type' => 'application/json',
+            'User-Agent' => 'Faraday v1.5.1'
+          }
+        )
+        .to_return(status: 422, body: '', headers: {})
+      expect { Api.send_data(data) }.to raise_error(Faraday::UnprocessableEntityError)
+    end
   end
 end
